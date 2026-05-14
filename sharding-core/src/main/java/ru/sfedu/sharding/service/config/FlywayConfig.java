7554e494a8e74afbc4f70739183c5c008f426442
wintermindset <wintermindset@yandex.ru>
2026-05-14 16:55:15 -0700

    fix: add Flyway manual config for Spring Boot 4.x and fix Docker build images

---
+++ sharding-core/src/main/java/ru/sfedu/sharding/service/config/FlywayConfig.java
@@ -0,0 +1,31 @@
+package ru.sfedu.sharding.service.config;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        if (beanFactory.containsBeanDefinition("entityManagerFactory")) {
+            var bd = beanFactory.getBeanDefinition("entityManagerFactory");
+            bd.setDependsOn("flyway");
+        }
+    }
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway(DataSource dataSource) {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load();
+    }
+}
