c139acbd5a9b7a82d881056771975a71ddb4883b
wintermindset <wintermindset@yandex.ru>
2026-05-14 15:57:10 -0700

    feat: configure Micrometer metrics with Prometheus (refs #15)

--- sharding-core/src/main/java/ru/sfedu/sharding/service/ShardingServiceApplication.java
+++ sharding-core/src/main/java/ru/sfedu/sharding/service/ShardingServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShardingServiceApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShardingServiceApplication.class, args);
 	}
