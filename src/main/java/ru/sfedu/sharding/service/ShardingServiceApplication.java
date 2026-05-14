b2d3a5fe2eb0bb64b42226143406182b881fc461
wintermindset <wintermindset@yandex.ru>
2026-05-14 00:00:05 -0700

    chore: initialize sharding-service

---
+++ src/main/java/ru/sfedu/sharding/service/ShardingServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.sfedu.sharding.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/sfedu/sharding/service/ShardingServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.sfedu.sharding.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
