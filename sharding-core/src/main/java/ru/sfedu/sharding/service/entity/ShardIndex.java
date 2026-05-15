d09b433d0a9f99a5dcca2a855db5ee48ade7dbd5
wintermindset <wintermindset@yandex.ru>
2026-05-14 19:34:04 -0700

    chore: update docker-compose

--- sharding-core/src/main/java/ru/sfedu/sharding/service/entity/ShardIndex.java
+++ sharding-core/src/main/java/ru/sfedu/sharding/service/entity/ShardIndex.java
@@ -8,19 +8,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.jspecify.annotations.Nullable;
 
 @Entity
 @Table(name = "shard_indices")
 @Getter
 @Setter
-@NoArgsConstructor
 public class ShardIndex {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private @Nullable Long id;
 
     @Column(name = "object_id", unique = true, nullable = false, length = 64)
     private String objectId;
@@ -30,7 +29,10 @@ public class ShardIndex {
 
     @Version
     @Column(name = "version")
-    private Long version;
+    private @Nullable Long version;
+
+    @SuppressWarnings("NullAway")
+    protected ShardIndex() {}
 
     public ShardIndex(String objectId, Integer shardIndex) {
         this.objectId = objectId;
